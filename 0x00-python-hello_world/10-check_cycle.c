c556b76119b7e59690d301f3e292adb25b8bb3f0
yassinm05 <yassinmahmoudmostafa@gmail.com>
2023-11-27 11:12:19 -0800

    task 10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * check_cycle - checks if there is a cycle in the linked list
+ * @list: pointer to list to be freed
+ * Return: 1 or 0
+ */
+
+int check_cycle(listint_t *list)
+{
+	if (list == NULL || list->next == NULL)
+	{
+		return (0);
+	}
+
+	listint_t *first;
+	listint_t *second;
+
+	first = list->next;
+	second = list->next->next;
+
+	while (first != NULL && second != NULL && second->next != NULL)
+	{
+		if (first == second)
+		{
+			return (1);
+		}
+		first = first->next;
+		second = second->next->next;
+	}
+	return (0);
+}
