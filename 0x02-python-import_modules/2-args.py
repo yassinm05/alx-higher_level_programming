fed50bd453eee282f82755849ae3b8a628b70a90
yassinm05 <yassinmahmoudmostafa@gmail.com>
2023-11-30 05:06:46 -0800

    task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    number = len(sys.argv) - 1
+    if number == 0:
+        print("0 arguments.")
+    elif number == 1:
+        print("1 argument:")
+        print("{}: {}".format(1, sys.argv[1]))
+    else:
+        print("{} arguments:".format(number))
+        for i in range(number):
+            print("{}: {}".format(i + 1, sys.argv[i + 1]))
