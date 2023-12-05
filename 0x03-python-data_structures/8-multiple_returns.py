0c1a602d81a22656592a51e626d0e1bde751e2f2
yassinm05 <yassinmahmoudmostafa@gmail.com>
2023-12-05 09:56:01 -0800

    task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if len(sentence == 0):
-        return (None)
+    if len(sentence) == 0:
+        return_val = (len(sentence), None)
+        return (return_val)
     else:
         return_val = (len(sentence), sentence[0])
         return (return_val)
