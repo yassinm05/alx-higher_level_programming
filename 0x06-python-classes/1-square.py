1669ff5e32612da82d77485394117f5a3a901837
yassinm05 <yassinmahmoudmostafa@gmail.com>
2023-12-20 15:58:49 -0800

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""sqaure module"""
+
+
+class Square:
+    """Defines a square"""
+    def __init__(self, size):
+        """constructor
+        args:
+            size: length of the side
+        """
+        self.__size = size
