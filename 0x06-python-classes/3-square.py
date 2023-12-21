f1ebd687a4504048f63b34e4fa1a335941432e3d
yassinm05 <yassinmahmoudmostafa@gmail.com>
2023-12-20 16:21:20 -0800

    task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""sqaure module"""
+
+
+class Square:
+
+    """Defines a square"""
+
+    def __init__(self, size=0):
+        """constructor
+        args:
+            size: length of the side
+        raises:
+            TypeError: if size is not an int
+            ValueError:if size is less than 0
+        """
+        if not type(size) == int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """area of the square
+        Returns:
+            the area of the square
+        """
+        return self.__size * self.__size
