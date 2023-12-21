07ff57e5b6fd2699eb58cca42e031af541d6cb3a
yassinm05 <yassinmahmoudmostafa@gmail.com>
2023-12-20 16:10:11 -0800

    task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -4,7 +4,7 @@
 
 class Square:
     """Defines a square"""
-    def __init__(self, size):
+    def __init__(self, size = 0):
         """constructor
         args:
             size: length of the side
