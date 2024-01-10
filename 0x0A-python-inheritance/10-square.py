b33a146277cad14ec8a1c5d7319062e51758a664
yassinm05 <yassinmahmoudmostafa@gmail.com>
2024-01-10 09:46:38 -0800

    task 9

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""module for empty class"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """a subclass OF A sub"""
+    def __init__(self, size):
+        """constructor"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area():
+        """return the area"""
+        return self.__size * self.__size
