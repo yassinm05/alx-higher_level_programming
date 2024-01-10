e8cce10987e600efab03a56d06376bf951f8aa3a
yassinm05 <yassinmahmoudmostafa@gmail.com>
2024-01-10 09:33:39 -0800

    task 9

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""module for empty class"""
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+
+    """a subclass"""
+    def __init__(self, width, height):
+        """constructor"""
+        self.integer_validator("width", width)
+        slef.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """return the area"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """representation"""
+        return "[Rectangle]" + str(self.__width) + "/" + str(self.__heigth)
