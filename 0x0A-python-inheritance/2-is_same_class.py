6fa34a393f43e43b1e0db855f90ade97cb7fe664
yassinm05 <yassinmahmoudmostafa@gmail.com>
2024-02-21 12:02:24 -0800

    task 2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""a module for is_same_class method"""
+
+
+def is_same_class(obj, a_class):
+    """return true if the object is the same class"""
+    return type(obj) == a_class
