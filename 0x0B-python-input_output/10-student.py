4c39dddcd40623fdbb8923705691fd97cc3b1b6f
yassinm05 <yassinmahmoudmostafa@gmail.com>
2024-02-20 18:06:03 -0800

    task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""class and json"""
+
+
+class Student:
+    """a student class"""
+    def __init__(self, first_name, last_name, age):
+        """init the student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """return the dict json"""
+        try:
+            for attribute in attrs:
+                if type(attribute) is not str:
+                    return self.__dict__
+        except Exception:
+            return self.__dict__
+        diction = dict()
+        for i, j in self.__dict__.items():
+            if i in attrs:
+                diction[i] = j
+        return diction
