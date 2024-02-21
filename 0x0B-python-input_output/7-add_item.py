50e0040af4ec949e913bb382ba0ae4b4ad8f66a0
yassinm05 <yassinmahmoudmostafa@gmail.com>
2024-02-20 17:41:20 -0800

    task 7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""a function that use json"""
+
+
+import sys
+save_to_json_file = __import__('5-save_to_json_file.py').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file.py').load_from_json_file
+
+
+argument = list(sys.argv[1:])
+
+try:
+    old_data = load_from_json_file('add_item.json')
+except Exception:
+    old_data = []
+
+old_data.extend(argument)
+save_to_json_file(old_data, 'add_item.json')
